644da57f73a082777ada397ab8aec3b905a7e9f0
Tiphnm <76214478+Tiphnm@users.noreply.github.com>
2021-02-15 12:17:55 +0100

    Utilisation mysql.connector et inclusion dans ma database

--- db.py
+++ db.py
@@ -1,43 +1,22 @@
-import sqlite3
-import pandas as pd
+import mysql.connector
 from webscrap import *
 
-
-conn = sqlite3.connect("my_db.db")
-c = conn.cursor()
+database = mysql.connector.connect(
+  host="scrap_sql",
+  database= "Maison_du_Monde",
+  user="root",
+  password="123", 
+)
 
 label_list = name_carpet()
 price_list = price_carpet()
 
-def delete_table():
-        c.execute("DROP TABLE carpet")
-        conn.commit()
+mycursor= database.cursor()
 
-delete_table()
+mycursor.execute("CREATE TABLE IF NOT EXISTS Carpet (ID INT PRIMARY KEY AUTO_INCREMENT, NOM VARCHAR(255), PRIX VARCHAR(30))")
+sql = "INSERT INTO Carpet (NOM, PRIX) VALUES (%s,%s)"
+valeur = final_carpet
+mycursor.executemany(sql, valeur)
 
-def create_db():
-    c.execute("CREATE TABLE IF NOT EXISTS carpet (ID INTEGER PRIMARY KEY, NOM VARCHAR(255) NOT NULL, PRIX VARCHAR(30) NOT NULL)")
-
-create_db()
-
-def insert_db():
-    c.executemany("INSERT INTO carpet (NOM, PRIX) VALUES(?, ?) ;", (final_carpet))
-    conn.commit()
-
-insert_db()
-
-def read_db():
-    Tableau = pd.read_sql_query("SELECT * from carpet", conn)
-    print(Tableau)
-
-    '''for row in c.execute("SELECT * FROM carpet"):
-        print(row)'''
-    conn.close()
-
-result = read_db()
-
-
-
-
-
+database.commit()
 
