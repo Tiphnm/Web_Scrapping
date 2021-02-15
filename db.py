73ced7d333837c6fb939ce3cbb08a767565ea8f5
Tiphnm <76214478+Tiphnm@users.noreply.github.com>
2021-02-15 16:39:13 +0100

    transformation en classe de mes fonctions pour créer ma database

--- db.py
+++ db.py
@@ -1,23 +1,26 @@
 import mysql.connector
 from webscrap import *
 
-database = mysql.connector.connect(
-  host="scrap_sql",
-  database= "Maison_du_Monde",
-  user="root",
-  password="123", 
-)
-mycursor= database.cursor()
+class Table: 
+    def __init__(self):
+        self.database = mysql.connector.connect(host="scrap_sql",database= "Maison_du_Monde",user="root",password="123")
+        self.mycursor= database.cursor()
 
-def create_table():
-    mycursor.execute("CREATE TABLE IF NOT EXISTS Carpet (ID INT PRIMARY KEY AUTO_INCREMENT, NOM VARCHAR(255), PRIX VARCHAR(30))")
-    database.commit()
-create_table()
+    def erase_table():
+        self.mycursor.execute("DROP table Carpet")
+        self.database.commit()
 
+    def create_table():
+        self.mycursor.execute("CREATE TABLE IF NOT EXISTS Carpet (ID INT PRIMARY KEY AUTO_INCREMENT, NOM VARCHAR(255), PRIX VARCHAR(30))")
+        self.database.commit()
 
-def insert_info():
-    sql = "INSERT INTO Carpet (NOM, PRIX) VALUES (%s,%s)"
-    valeur = final_carpet
-    mycursor.executemany(sql, valeur)
-    database.commit()
-insert_info()
+    def insert_info():
+        sql = "INSERT INTO Carpet (NOM, PRIX) VALUES (%s,%s)"
+        valeur = c.zip_list()
+        self.mycursor.executemany(sql, valeur)
+        self.database.commit()
+
+t = Table()
+t.erase_table()
+t.create_table()
+t.insert_info()
